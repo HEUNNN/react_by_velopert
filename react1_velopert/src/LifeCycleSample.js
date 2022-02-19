d069c83bb401006e40c9142aa064ad4fe279b5d6
leehyeeun <leehe0430@gmail.com>
2022-02-20 00:45:11 +0900

    2022/2/20

---
+++ react1_velopert/src/LifeCycleSample.js
@@ -0,0 +1,49 @@
+import { Component } from "react";
+
+class LifeCycleSample extends Component {
+  state = {
+    number: 0,
+    color: null,
+  };
+  myRef = null; //ref를 설정할 부분
+
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("getDerivedFromProps");
+    if (nextProps.color !== prevState.color) {
+      return { color: nextProps.color };
+    }
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate", nextProps, nextState);
+    return nextState.number % 10 !== 4;
+    //숫자의 마지막 자리가 4면 리렌더링하지 않습니다.
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  handleClick = () => {
+    this.setState({
+      number: this.state.number + 1,
+    });
+  };
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate");
+    if (prevProps.color !== this.props.color) {
+      return this.myRef.style.color;
+    }
+    return null;
+  }
+}
