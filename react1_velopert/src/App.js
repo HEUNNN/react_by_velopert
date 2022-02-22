4db4f85dfd6be996a8382e3de8eea41d6ffc7564
leehyeeun <leehe0430@gmail.com>
2022-02-22 22:27:42 +0900

    2022/2/22 lifecycle

--- react1_velopert/src/App.js
+++ react1_velopert/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Fragment } from "react";
+import { Component } from "react";
 import Counter from "./Counter";
 import Password from "./Password";
 import Scroll from "./Scroll";
@@ -7,22 +7,45 @@ import { useRef } from "react";
 import IterationSample from "./IterationSample";
 import IterationSampleTwo from "./IterationSampleTwo";
 import Validation from "./Validation";
+import LifeCycle from "./LifeCycle";
+import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-      <Password />
-      <h3>Scroll Exercise</h3>
-      <Scroll />
-      <h3>Validation</h3>
-      <Validation />
-      <h3>Iteration Sample(1)</h3>
-      <IterationSample />
-      <h3>Iteration Sample 응용(2)</h3>
-      <IterationSampleTwo />
-    </div>
-  );
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Counter />
+        <Password />
+        <h3>Scroll Exercise</h3>
+        <Scroll />
+        <h3>Validation</h3>
+        <Validation />
+        <h3>Iteration Sample(1)</h3>
+        <IterationSample />
+        <h3>Iteration Sample 응용(2)</h3>
+        <IterationSampleTwo />
+        <br />
+        <h3>Life Cycle</h3>
+        <div>
+          <button onClick={this.handleClick}>랜덤 색상</button>
+          <LifeCycle color={this.state.color} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
