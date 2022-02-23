ffbc3211fe8964623ca519acdcc62120a81774fc
leehyeeun <leehe0430@gmail.com>
2022-02-23 18:16:48 +0900

    2022/2/23 useEffect()

--- hooks_sample/hooks_sample/src/App.js
+++ hooks_sample/hooks_sample/src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
 import Counter from "./Counter";
+import { useState } from "react";
 import Info from "./Info";
 function App() {
+  const [isVisible, setIsVisible] = useState(true);
   return (
     <div className="App">
       <h3>Counter</h3>
       <Counter />
       <br />
-      <h3>Information input</h3>
-      <Info />
+      <hr />
+      <h3>Information input(isVisible)</h3>
+      <button
+        onClick={() => {
+          setIsVisible(!isVisible);
+        }}
+      >
+        {isVisible ? "숨기기" : "보이기"}
+      </button>
+      <br />
+      <br />
+      {isVisible && <Info />}
+      <hr />
     </div>
   );
 }
--- hooks_sample/hooks_sample/src/Info.js
+++ hooks_sample/hooks_sample/src/Info.js
@@ -1,9 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Info = () => {
   //Info component 내부
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+
+  useEffect(() => {
+    console.log("렌더링이 완료되었습니다.");
+    console.log({ name, nickname });
+  });
+
+  useEffect(() => {
+    console.log("Mount되었습니다.");
+  }, []);
+
+  useEffect(() => {
+    console.log("effect(update)");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
+  }, [name]);
+
+  useEffect(() => {
+    console.log("effect(update)");
+    return () => {
+      console.log("cleanup only unmount");
+    };
+  }, []);
+
   const onHandleName = (e) => {
     setName(e.target.value);
   };
