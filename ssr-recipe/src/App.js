e3b6c730bf93c885cf4386207cbcdd8b824b4d90
leehyeeun <leehe0430@gmail.com>
2022-03-25 18:53:33 +0900

    React Velopert CH20 Sever Side Rendering - 준비

---
+++ ssr-recipe/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Menu from "./components/Menu";
+import BluePage from "./pages/BluePage";
+import RedPage from "./pages/RedPage";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Menu />
+        {/* <Routes> 를 벗어난 부분에 작성되는 코드는 페이지가 변해도 그대로 유지된다.*/}
+        <hr />
+        <Routes>
+          <Route path="/red" element={<RedPage />} />
+          <Route path="/blue" element={<BluePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
